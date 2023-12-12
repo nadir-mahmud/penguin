bc4dd13fe41361317c6aa55c10aa04d1ae2b7c5c
nadir-mahmud <nadirmahmud5@gmail.com>
2023-12-12 21:12:04 +0600

    flowbite 2.2

--- frontend/src/components/AuthHeader.jsx
+++ frontend/src/components/AuthHeader.jsx
@@ -90,7 +90,7 @@ const AuthHeader = () => {
               type="button"
               data-collapse-toggle="navbar-search"
               aria-controls="navbar-search"
-              aria-expanded="false"
+              aria-expanded="true"
               className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
             >
               <svg
