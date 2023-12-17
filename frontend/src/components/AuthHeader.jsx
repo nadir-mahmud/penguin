2699f6c1d6e1cf193c8cd740b56d0088e2fc0f82
nadir-mahmud <nadirmahmud5@gmail.com>
2023-12-17 11:59:05 +0600

    link color fix

--- frontend/src/components/AuthHeader.jsx
+++ frontend/src/components/AuthHeader.jsx
@@ -213,7 +213,7 @@ const AuthHeader = () => {
                 <NavLink
                   onClick={handleHomeLink}
                   to="/"
-                  className="block py-2 pl-3 pr-4 text-white  rounded md:bg-transparent md:text-white  md:focus:text-rose-300 md:p-0 md:dark:text-white"
+                  className="block py-2 pl-3 pr-4 text-white rounded hover:bg-purple-500 md:hover:bg-transparent md:text-white  md:focus:text-purple-800 md:p-0 md:dark:text-white"
                   aria-current="page"
                 >
                   Home
@@ -223,7 +223,7 @@ const AuthHeader = () => {
                 <NavLink
                   onClick={handleOrderLink}
                   to="/order"
-                  className="block py-2 pl-3 pr-4 text-white  rounded md:bg-transparent md:text-white  md:focus:text-rose-300 md:p-0 md:dark:text-white"
+                  className="block py-2 pl-3 pr-4 text-white rounded hover:bg-purple-500 md:hover:bg-transparent md:text-white  md:focus:text-purple-800 md:p-0 md:dark:text-white"
                   aria-current="page"
                 >
                   Order
@@ -234,7 +234,7 @@ const AuthHeader = () => {
                 <NavLink
                   onClick={handleCartLink}
                   to="/cart"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-white md:hover:bg-transparent md:focus:text-rose-300 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 text-white rounded hover:bg-purple-500  md:text-white md:hover:bg-transparent md:focus:text-purple-800 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Cart
                 </NavLink>
@@ -243,7 +243,7 @@ const AuthHeader = () => {
                 <NavLink
                   onClick={handleLogout}
                   to="/"
-                  className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:focus:text-rose-300 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 text-white rounded hover:bg-purple-500 md:hover:bg-transparent md:focus:text-purple-800 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Logout
                 </NavLink>
--- frontend/src/components/InitialHeader.jsx
+++ frontend/src/components/InitialHeader.jsx
@@ -210,7 +210,7 @@ const InitialHeader = () => {
                 <NavLink
                   onClick={handleHomeLink}
                   to="/"
-                  className="block py-2 pl-3 pr-4 text-white  rounded md:bg-transparent md:text-white  md:focus:text-rose-300 md:p-0 md:dark:text-white"
+                  className="block py-2 pl-3 pr-4 text-white  rounded hover:bg-purple-500 md:hover:bg-transparent md:text-white  md:focus:text-purple-800 md:p-0 md:dark:text-white"
                   aria-current="page"
                 >
                   Home
@@ -220,7 +220,7 @@ const InitialHeader = () => {
                 <NavLink
                   onClick={handleLoginLink}
                   to="/login"
-                  className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:focus:text-rose-300 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 text-white rounded hover:bg-purple-500 md:hover:bg-transparent md:focus:text-purple-800 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Login
                 </NavLink>
@@ -229,7 +229,7 @@ const InitialHeader = () => {
                 <NavLink
                   onClick={handleRegisterLink}
                   to="/register"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-white md:hover:bg-transparent md:focus:text-rose-300 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 text-white rounded hover:bg-purple-500 md:text-white md:hover:bg-transparent md:focus:text-purple-800 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Register
                 </NavLink>
@@ -237,7 +237,7 @@ const InitialHeader = () => {
               <li>
                 <NavLink
                   to="/login"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-white md:hover:bg-transparent md:focus:text-rose-300 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 text-white rounded hover:bg-purple-500 md:text-white md:hover:bg-transparent md:focus:text-purple-800 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Cart
                 </NavLink>
